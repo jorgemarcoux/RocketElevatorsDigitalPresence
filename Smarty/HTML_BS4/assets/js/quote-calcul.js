0a2f5a41cdeb86d2c9f1130ac201647cf10e2f6d
jorgemarcoux <jorgemarcoux@gmail.com>
2020-09-08 14:19:33 -0400

    JS commencé

---
+++ Smarty/HTML_BS4/assets/js/quote-calcul.js
@@ -0,0 +1,15 @@
+
+/*
+$("#type-building").click(function(){
+	$(this).hide();
+	});
+*/
+var typeB = $("#num-apart").val();
+var resiQues = $("#res-ques");
+var test = $("#test").val()
+
+if($("#num-apart").val() == "res"){
+	resiQues.addClass("d-none");
+};
+
+console.log(test);
