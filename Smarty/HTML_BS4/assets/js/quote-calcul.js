042bdf9538ec1355286761c88b38cdf5e1ba04e6
jorgemarcoux <jorgemarcoux@gmail.com>
2020-09-09 12:21:49 -0400

    JS = residential calculation done

--- Smarty/HTML_BS4/assets/js/quote-calcul.js
+++ Smarty/HTML_BS4/assets/js/quote-calcul.js
@@ -33,4 +33,34 @@ $(function(){
 })
 });
 
+/*input event para agarrar input de preguntas extras residencia. 
+Poner esa input en variables y luego hacer calculos. Todo eso
+dentro de una funcion SOLO para resis. El resultado de cage
+debe ser lanzado en el only-read input*/
 
+/*
+$(function(){
+  $("#num-apart").on("input", function(){
+  var apartR = $(this).val(); 
+   
+});
+});
+
+*/
+$(function(){
+  $("#num-apart").on("input", function(){
+  var apartR = $(this).val();
+  $("#num-floors").on("input", function(){
+  var floorR = $(this).val();
+  $("#num-bas").on("input", function(){
+  var basR = $(this).val();
+
+  var resR = apartR/floorR;
+  $("#cages").removeClass("d-none");
+  $("#resultado").val("Recommended number of elevators: " + resR);
+  
+   
+});
+});
+});
+});
