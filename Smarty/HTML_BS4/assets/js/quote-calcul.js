98f0092c1a6452eabcda966efc3366f8ef4f4740
jorgemarcoux <jorgemarcoux@gmail.com>
2020-09-08 20:27:14 -0400

    News section finished, all blog posts finished, progress in JS form

--- Smarty/HTML_BS4/assets/js/quote-calcul.js
+++ Smarty/HTML_BS4/assets/js/quote-calcul.js
@@ -1,15 +1,36 @@
 
-/*
-$("#type-building").click(function(){
-	$(this).hide();
-	});
-*/
-var typeB = $("#num-apart").val();
 var resiQues = $("#res-ques");
-var test = $("#test").val()
 
-if($("#num-apart").val() == "res"){
-	resiQues.addClass("d-none");
-};
+//Show questions
+$(function(){ 
+ $("#type-building").change(function(){
+ var building = $("#type-building option:selected").text();
+  if(building == "Residential"){
+  	resiQues.removeClass("d-none");
+  	$("#commer-ques").addClass("d-none");
+  	$("#corp-ques").addClass("d-none");
+  	$("#hybrid-ques").addClass("d-none");
+  }
+  else if(respuesta == "Commercial"){
+      $("#commer-ques").removeClass("d-none");
+      resiQues.addClass("d-none");
+  	  $("#corp-ques").addClass("d-none");
+  	  $("#hybrid-ques").addClass("d-none");
+      
+  }
+  else if(respuesta =="Corporate"){
+  	$("#corp-ques").removeClass("d-none");
+  	resiQues.addClass("d-none");
+  	$("#commer-ques").addClass("d-none");
+  	$("#hybrid-ques").addClass("d-none");
+  }
+  else{
+  	$("#hybrid-ques").removeClass("d-none");
+  	resiQues.addClass("d-none");
+  	$("#corp-ques").addClass("d-none");
+  	$("#commer-ques").addClass("d-none");
+  }
+})
+});
 
-console.log(test);
+
